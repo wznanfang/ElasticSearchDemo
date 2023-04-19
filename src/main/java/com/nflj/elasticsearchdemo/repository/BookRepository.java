5902e6a0234938a82c614a355a21c578011a9d24
南方 <75766381+wznanfang@users.noreply.github.com>
2023-04-19 09:52:44 +0800

    Update BookRepository.java

--- src/main/java/com/nflj/elasticsearchdemo/repository/BookRepository.java
+++ src/main/java/com/nflj/elasticsearchdemo/repository/BookRepository.java
@@ -2,11 +2,13 @@ package com.nflj.elasticsearchdemo.repository;
 
 import com.nflj.elasticsearchdemo.domain.Book;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author zp.wei
  * @date 2021/6/28 15:34
  */
-
+@Repository
 public interface BookRepository extends ElasticsearchRepository<Book, String> {
 }
+
