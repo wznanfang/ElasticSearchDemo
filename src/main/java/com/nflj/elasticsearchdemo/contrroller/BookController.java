67a90210364f82d36bd0a93e480d961ab9e0db10
南方 <75766381+wznanfang@users.noreply.github.com>
2023-04-19 09:51:49 +0800

    fix: 修改

--- src/main/java/com/nflj/elasticsearchdemo/contrroller/BookController.java
+++ src/main/java/com/nflj/elasticsearchdemo/contrroller/BookController.java
@@ -3,11 +3,10 @@ package com.nflj.elasticsearchdemo.contrroller;
 import com.nflj.elasticsearchdemo.domain.Book;
 import com.nflj.elasticsearchdemo.repository.BookRepository;
 import com.nflj.elasticsearchdemo.vo.BookVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -25,28 +24,15 @@ public class BookController {
     @PostMapping("/save")
     public Object save(@RequestBody BookVo bookVo) {
         Book book = new Book();
-        if (StringUtils.hasLength(bookVo.getName())) {
-            book.setName(book.getName());
-        }
-        if (StringUtils.hasLength(bookVo.getAuthor())) {
-            book.setAuthor(book.getAuthor());
-        }
-        if (StringUtils.hasLength(bookVo.getPrice().toString())) {
-            book.setPrice(book.getPrice());
-        }
-        if (StringUtils.hasLength(bookVo.getIntroduction())) {
-            book.setIntroduction(book.getIntroduction());
-        }
-        Book book1 = bookRepository.save(book);
-        return book1;
+        BeanUtils.copyProperties(bookVo, book);
+        return bookRepository.save(book);
     }
 
 
     @GetMapping("/findAll")
     public Object findAll() {
         Pageable pageable = PageRequest.of(0, 10);
-        Page<Book> page = bookRepository.findAll(pageable);
-        return page;
+        return bookRepository.findAll(pageable);
     }
 
 
