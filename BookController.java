e63e2ed8fcab80cd4b14c66462b565d1bdc532ae
南方 <75766381+wznanfang@users.noreply.github.com>
2023-04-19 09:49:59 +0800

    修改

---
+++ BookController.java
@@ -0,0 +1,39 @@
+package com.nflj.elasticsearchdemo.contrroller;
+
+import com.nflj.elasticsearchdemo.domain.Book;
+import com.nflj.elasticsearchdemo.repository.BookRepository;
+import com.nflj.elasticsearchdemo.vo.BookVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author zp.wei
+ * @date 2021/6/28 15:39
+ */
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+
+    @PostMapping("/save")
+    public Object save(@RequestBody BookVo bookVo) {
+        Book book = new Book();
+        BeanUtils.copyProperties(bookVo, book);
+        return bookRepository.save(book);
+    }
+
+
+    @GetMapping("/findAll")
+    public Object findAll() {
+        Pageable pageable = PageRequest.of(0, 10);
+        return bookRepository.findAll(pageable);
+    }
+
+
+}
